34cb6586dd180f14b64b5ae5f2c9885f073270ca
xxx-sj <loky13@daum.net>
2024-07-10 16:11:36 +0900

    init: init

---
+++ src/main/java/sample/convention/ConventionApplication.java
@@ -0,0 +1,13 @@
+package sample.convention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConventionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sample/convention/ConventionApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.convention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConventionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
