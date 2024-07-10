59766bbe51599ecfc888a69f7e63335b9b7c3043
xxx-sj <loky13@daum.net>
2024-07-10 16:23:54 +0900

    feat: add property
    add property id in hello object
    
    Fixes: #2

--- src/main/java/sample/convention/Hello.java
+++ src/main/java/sample/convention/Hello.java
@@ -1,4 +1,15 @@
 package sample.convention;
 
 public class Hello {
+
+    private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
